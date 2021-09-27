c71644846e638b81230bd57d6abea97641934a9c
ShowTimeWalker <showtimewalker@163.com>
2021-09-27 23:09:59 +0800

    serial interact with matlab

--- Matlab/SerialTransmit/SerialTransmit.ino
+++ Matlab/SerialTransmit/SerialTransmit.ino
@@ -20,7 +20,7 @@ void setup() {
   xTaskCreate(
     writeToPort,
     "PrintToSerial",
-    128,  // stack size
+    256,  // stack size
     NULL, // parameters
     1,    // priority
     NULL  // handle 
@@ -43,11 +43,16 @@ void blink(void *pvParameters __attribute__((unused))) {
 }
 
 void writeToPort(void *pvParameters) {
-  int counter = 0;
+  int a = 5;
+  int b = 1;
+  char msg[7] = "";
   while(true) {
-    Serial.print(counter++);
-    Serial.print(": ");
-    Serial.println("hello world");
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    a += 1;
+    b += 2;
+    sprintf(msg, "%03d,%03d", a % 1000, b % 1000);
+    Serial.write(msg);
+    Serial.write(13);
+    Serial.write(10);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
